6a5c79a771d5a85e259c6aa6cb0fcb91db84e3da
KhaleelKazy <khaleelgoldsby@gmail.com>
2026-09-06 14:36:08 -0500

    commit 5
    commit phase 5

--- Journal.md
+++ Journal.md
@@ -7,3 +7,10 @@ When I tried to log defaultTaxRate I get a ReferenceError of not defined it happ
 Journal Prompt 3
 The arrow replaces the fuction keyword. Arrow functions also skip curly braces. The code becomes easier to read.
 
+
+Journal Prompt 4
+applyDiscount is considered a Higher-Order Function it works with other functions studentDiscount and seniorDiscount are callback functions
+if you want to apply no discount you make a function with no retur
+
+Journal Prompt 5
+I broke order process by only having the function be correct once. If i have to do the math again i just call the function
--- script.js
+++ script.js
@@ -21,3 +21,25 @@ function calculateStoreTax(amount) {
 }
 
 console.log(calculateStoreTax(100));
+
+const applyDiscount = (total, discountCallback) => {
+  return discountCallback(total);
+};
+
+function studentDiscount(total) {
+  return total * 0.9;
+}
+
+function seniorDiscount(total) {
+  return total * 0.8;
+}
+
+console.log(applyDiscount(100, studentDiscount));
+console.log(applyDiscount(100, seniorDiscount));
+
+function calculateFinalBill(price, quantity, discountCallback) {
+    let subtotal = calculatePrice(price, quantity);
+    let discountedPrice = applyDiscount(subtotal, discountCallback);
+    let taxAmount = getTax(discountedPrice, 0.07);
+    return discountedPrice + taxAmount;
+}
